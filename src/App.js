0904dba81096bf0e6e86a9bb114bd35f7f9f2a77
Somyajeet Gupta Chowdhury <somya@Somyajeets-MacBook-Pro.local>
2024-08-28 02:20:21 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import "./App.css";
-import { MultiStepLoader } from "./components/ui/multi-step-loader.jsx";
-import { Timeline } from "./components/ui/timeline.jsx";
 import "./index.css";
-import { ExpandCard } from "./pages/ExpandCard.jsx";
-import { GeminiEffect } from "./pages/GeminiEffect.jsx";
-import { HoverEffect } from "./pages/HoverEffect.jsx";
-import { MultiStepLoaderDemo } from "./pages/MultiStepLogin.jsx";
-import { Navbar } from "./pages/Navbar.jsx";
-import { Scroll } from "./pages/ScrollAnimation.jsx";
-import { SignupFormDemo } from "./pages/Signup.jsx";
-import { TimelineDemo } from "./pages/Timeline.jsx";
-
 
 function App() {
-  return (
-    <div>
-    
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
