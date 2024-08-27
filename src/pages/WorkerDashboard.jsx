95527c6aa8b8bb302d9c8cb25d36ce7be1fe6e92
Somyajeet Gupta Chowdhury <somya@Somyajeets-MacBook-Pro.local>
2024-08-28 02:07:13 +0530

    my changes

--- src/pages/WorkerDashboard.jsx
+++ src/pages/WorkerDashboard.jsx
@@ -4,7 +4,7 @@ import { ScrollView } from "./ScrollView";
 const WorkerDashboard = () => {
   return (
     <div className="flex gap-3">
-      <div className="left-0 w-[15vw] h-screen sticky">left panel</div>
+      <div className="left-0 w-[15vw] h-screen overflow-none">left panel</div>
       <div className="w-[60vw]">
         <ScrollView />
       </div>
