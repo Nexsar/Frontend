75db9c6bdd435bbeec1c5cd0eb730965fb6a7fed
unknown <jyothiraditya29@example.com>
2024-08-28 01:41:53 +0530

    fix::> Basic UI is ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { MultiStepLoader } from "./components/ui/multi-step-loader.jsx";
+import { Timeline } from "./components/ui/timeline.jsx";
 import "./index.css";
+import { ExpandCard } from "./pages/ExpandCard.jsx";
+import { GeminiEffect } from "./pages/GeminiEffect.jsx";
+import { HoverEffect } from "./pages/HoverEffect.jsx";
+import { MultiStepLoaderDemo } from "./pages/MultiStepLogin.jsx";
 import { Navbar } from "./pages/Navbar.jsx";
+import { Scroll } from "./pages/ScrollAnimation.jsx";
+import { SignupFormDemo } from "./pages/Signup.jsx";
+import { TimelineDemo } from "./pages/Timeline.jsx";
+
 
 function App() {
   return (
-    <div className="flex justify-center text-red-500">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Navbar />
+    <div>
+    
     </div>
   );
 }
