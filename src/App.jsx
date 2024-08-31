274880c9c2cf37974bcae3861cf7b9b888ab0b6d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-09-01 02:22:11 +0530

    fix::> Added missing dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WorkerDashboard from "./pages/WorkerDashboard.jsx";
 import Home from "./pages/Home.jsx";
 import DistributorDashboard from "./pages/DistributorDashboard.jsx";
-import LitPage from "./lit/index";
+import SignUpView from "./lit/pages/index.jsx";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/worker" element={<WorkerDashboard />} />
           <Route path="/distributor" element={<DistributorDashboard />} />
-          <Route path="/lit" element={<LitPage />} />
+          <Route path="/lit" element={<SignUpView />} />
         </Routes>
       </BrowserRouter>
     </div>
