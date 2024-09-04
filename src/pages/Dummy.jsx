60db7f20f951da38725b0bcbbbbc1d46fbb23f93
somya-weilliptic <somya@weilliptic.com>
2024-09-05 02:51:30 +0530

    added dummy api call

--- src/pages/Dummy.jsx
+++ src/pages/Dummy.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { ethers } from "ethers";
+import { useState } from "react";
 
 const Dummy = () => {
+  const [signer, setSigner] = useState(null);
   useEffect(() => {
     const getsigner = async () => {
       console.log("window ka ethereum", window.ethereum);
@@ -11,7 +13,32 @@ const Dummy = () => {
 
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
+
         console.log(signer);
+        const url = "http://localhost:8000/dummy";
+        const data = {
+          signer,
+        };
+        fetch(url, {
+          method: "POST", // HTTP method
+          headers: {
+            "Content-Type": "application/json", // Specify that the content is JSON
+            // Add other headers if necessary
+          },
+          body: JSON.stringify(data), // Convert the data to a JSON string
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json(); // Parse JSON response
+          })
+          .then((result) => {
+            console.log("Success:", result);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
       } catch (err) {
         console.log("error happened in getting signer", err);
       }
